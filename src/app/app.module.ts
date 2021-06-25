9e10ecac93d099f00c4c2e79347bdb09809a323a
Junior Zadith Sierra Vasquez <jsierra_pr@sefin.gob.hn>
2021-06-25 11:02:51 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+//Modulos
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TareasComponent } from './components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TareasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
